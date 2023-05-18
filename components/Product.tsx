160120fed220069295c5875a7049b463517b6b1d
Andrew <andrewdebyl@gmail.com>
2023-05-18 16:48:40 -0400

    final

--- components/Product.tsx
+++ components/Product.tsx
@@ -8,7 +8,6 @@ type Props = {
 function Product({ product }: Props) {
   return (
     <Link
-      prefetch={false}
       href={`/product/${product.id}`}
       className="h-96 flex flex-col p-5 rounded border group hover:scale-105 transition-transform ease-out duration-200"
     >
